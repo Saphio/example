96e29eb380dce61c44ab3ac39f04993d7ae898f5
sophia <461932@bsd48.org>
2024-08-30 09:44:24 -0700

    add labels

--- example.cpp
+++ example.cpp
@@ -5,8 +5,9 @@ using namespace std;
 int main ()
 {
   int input = 0;
+  cout << "current number: " << input << endl;
   cin >> input;
-  cout << input;
+  cout << "you entered: " << input << endl;
 
   return 0;
 }
