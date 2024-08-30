eaed15250f9896ab26e35fc6fc7ae594dcaef10c
sophia <461932@bsd48.org>
2024-08-30 10:09:44 -0700

    minor changes

--- example.cpp
+++ example.cpp
@@ -6,14 +6,16 @@ int main ()
 {
   int input = 0;
   cin >> input;
- cout << input << endl;
+ cout << "Before loop: " << input << endl;
   
  while (input == 5) {
    cin >> input;
    cout << input << endl;
  }
+
  
- cout << input << endl;
+ 
+ cout << "After loop: " << input << endl;
 
  return 0;
 
