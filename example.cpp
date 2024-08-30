ac54e33419019120b9327ee9de267bf4cb7a21ee
sophia <461932@bsd48.org>
2024-08-30 10:04:50 -0700

    using loops

--- example.cpp
+++ example.cpp
@@ -6,19 +6,15 @@ int main ()
 {
   int input = 0;
   cin >> input;
-
- if (input == 3)
-   {
-     cout << "You entered a 3." << endl;
-     input++;
-   }
- else if (input == 4) {
-   cout << "You entered a 4." << endl;
- }
- else {
-   cout << "You entered something else." << endl;
+ cout << input << endl;
+  
+ while (input == 5) {
+   cin >> input;
+   cout << input << endl;
  }
  
-  cout << input << endl;
-  return 0;
+ cout << input << endl;
+
+ return 0;
+
 }
